db7a8226c2ab681e1c11f9216d5c09e0f3c8c240
Nataliia Orlova <nataliia.orlova.dev@gmail.com>
2024-01-25 17:04:31 +0100

    add edit, delete functionality

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,9 +1,25 @@
 //  to make sure the functions exported from this file are treated as SERVER ACTIONS =>
 'use server';
+import { redirect } from 'next/navigation';
 
 import { db } from '@/db';
 
 //  define server action
-export async function editSnippet() {
-    console.log('edit snippet called');
+export async function editSnippet(id: number, code: string) {
+    await db.snippet.update({
+        where: { id },
+        data: { code },
+    });
+
+    redirect(`/snippets/${id}`);
+}
+
+export async function deleteSnippet(id: number) {
+    await db.snippet.delete({
+        where: {
+            id,
+        },
+    });
+
+    redirect('/');
 }
--- src/components/snipet-edit-form.tsx
+++ src/components/snipet-edit-form.tsx
@@ -15,6 +15,7 @@ export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
         setCode(value);
     };
 
+    const editSnippetAction = actions.editSnippet.bind(null, snippet.id, code);
     return (
         <div>
             <Editor
@@ -25,6 +26,11 @@ export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
                 options={{ minimap: { enabled: false } }}
                 onChange={handleEditorChange}
             />
+            <form action={editSnippetAction}>
+                <button type='submit' className='p-2 border rounded'>
+                    Save
+                </button>
+            </form>
         </div>
     );
 }
