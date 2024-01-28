0938619520c08ad2a1876468da917a87bee955a1
Nataliia Orlova <nataliia.orlova.dev@gmail.com>
2024-01-28 22:12:58 +0100

    revert commit

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,25 +1,9 @@
 //  to make sure the functions exported from this file are treated as SERVER ACTIONS =>
 'use server';
-import { redirect } from 'next/navigation';
 
 import { db } from '@/db';
 
 //  define server action
-export async function editSnippet(id: number, code: string) {
-    await db.snippet.update({
-        where: { id },
-        data: { code },
-    });
-
-    redirect(`/snippets/${id}`);
-}
-
-export async function deleteSnippet(id: number) {
-    await db.snippet.delete({
-        where: {
-            id,
-        },
-    });
-
-    redirect('/');
+export async function editSnippet() {
+    console.log('edit snippet called');
 }
--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -2,7 +2,7 @@
 import { db } from '@/db';
 import { notFound } from 'next/navigation';
 import Link from 'next/link';
-import * as actions from '@/actions';
+
 interface SnippetShowPageProps {
     params: {
         id: string;
@@ -22,8 +22,6 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
         return notFound();
     }
 
-    const deleteSnippetAction = actions.deleteSnippet.bind(null, snippet.id);
-
     return (
         <div>
             <div className='flex m-4 justify-between items-center'>
@@ -36,12 +34,7 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
                     >
                         Edit
                     </Link>
-                    <form action={deleteSnippetAction}>
-                        <button className='p-2 border rounded'>Delete</button>
-                    </form>
-                    <Link href='/' className='p-2 border rounded'>
-                        View all snippets
-                    </Link>
+                    <button className='p-2 border rounded'>Delete</button>
                 </div>
             </div>
             <pre className='p-3 border rounded bg-gray-200'>
